1a297d46fe36bf5632ffb325b0ef54689b0942b2
Will <doggy.huang@gmail.com>
2022-11-19 02:40:52 +0800

    Add project files.

---
+++ FBAngularTW/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Run(ctx =>
+{
+    switch (ctx.Request.Host.Host)
+    {
+        case "fb.angular.tw":
+            ctx.Response.Redirect("https://www.facebook.com/groups/augularjs.tw");
+            break;
+
+        case "yt.angular.tw":
+            ctx.Response.Redirect("https://www.youtube.com/c/AngularUserGroupTaiwan/videos");
+            break;
+
+        default:
+            ctx.Response.Redirect("https://www.facebook.com/groups/augularjs.tw");
+            break;
+    }
+    return Task.CompletedTask;
+});
+
+app.Run();
