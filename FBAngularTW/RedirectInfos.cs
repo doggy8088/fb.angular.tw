9212631e9f8d40426165ecf1a587bdc4a674c6cc
TommyLin <wsx9876777@gmail.com>
2023-08-07 21:56:45 +0800

    t

---
+++ FBAngularTW/RedirectInfos.cs
@@ -0,0 +1,15 @@
+﻿namespace FBAngularTW
+{
+
+    public class RedirectInfos
+    {
+        public string DefaultUrl { get; set; }
+        public Redirectlist[] RedirectList { get; set; }
+    }
+
+    public class Redirectlist
+    {
+        public string Host { get; set; }
+        public string Url { get; set; }
+    }
+}
