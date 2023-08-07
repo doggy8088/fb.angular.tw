48e91520e4a3f4ce765fb4b618e714bbfd015378
王彥驊 <terry.wang@iisigroup.com>
2023-08-07 20:15:16 +0800

    網址轉換邏輯參數化
    將 switch case 中的域名與網址都改從 appsettings.json 讀取設定
    使用 Options pattern 用強型別的方式讀取參數
    短網址必須隨時可以擴充，不需要重新建置專案就能新增域名與網址對應

---
+++ FBAngularTW/DomainUrlEntry.cs
@@ -0,0 +1,18 @@
+﻿namespace FBAngularTW
+{
+    public class DomainUrlEntry
+    {
+        public string Domain { get; set; }
+
+        public string Url { get; set; }
+    }
+
+    public class DomainUrlConfig
+    {
+        public List<DomainUrlEntry> Entries
+        {
+            get; set;
+        }
+    }
+
+}
--- FBAngularTW/Program.cs
+++ FBAngularTW/Program.cs
@@ -1,42 +1,31 @@
+using FBAngularTW;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+services.Configure<DomainUrlConfig>(options => configuration.GetSection("DomainUrlConfigs").Bind(options));
 var app = builder.Build();
 
 app.Run(ctx =>
 {
-    switch (ctx.Request.Host.Host)
+    var serviceScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+    using var serviceScope = serviceScopeFactory.CreateScope();
+    IOptionsSnapshot<DomainUrlConfig> domainUrlConfigOption = serviceScope.ServiceProvider.GetRequiredService<IOptionsSnapshot<DomainUrlConfig>>();
+
+    var domainUrlConfig = domainUrlConfigOption.Value;
+
+    var targetDomainUr = domainUrlConfig.Entries.SingleOrDefault(x => x.Domain == ctx.Request.Host.Host);
+    if (targetDomainUr == null)
     {
-        case "fb.angular.tw":
-            ctx.Response.Redirect("https://www.facebook.com/groups/augularjs.tw");
-            break;
-
-        case "yt.angular.tw":
-            ctx.Response.Redirect("https://www.youtube.com/c/AngularUserGroupTaiwan/videos");
-            break;
-            
-        case "ts.angular.tw":
-            ctx.Response.Redirect("https://willh.gitbook.io/typescript-tutorial");
-            break;
-
-        case "vscode.angular.tw":
-            ctx.Response.Redirect("https://marketplace.visualstudio.com/items?itemName=doggy8088.angular-extension-pack");
-            break;
-
-        case "cli.angular.tw":
-            ctx.Response.Redirect("https://youtu.be/v4_YsDZbs3g");
-            break;
-
-        case "rx6.angular.tw":
-            ctx.Response.Redirect("https://youtu.be/BA1vSZwzkK8");
-            break;
-
-        case "install.angular.tw":
-            ctx.Response.Redirect("https://gist.github.com/doggy8088/15e434b43992cf25a78700438743774a");
-            break;
-
-        default:
-            ctx.Response.Redirect("https://www.facebook.com/will.fans");
-            break;
+        ctx.Response.Redirect("https://www.facebook.com/will.fans");
     }
+    else
+    {
+        ctx.Response.Redirect($"{targetDomainUr.Url}");
+    }
+   
     return Task.CompletedTask;
 });
 
