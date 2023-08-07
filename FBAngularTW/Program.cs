a06d3550bd9323fa8b9e40a0cc875842e8b2bf48
MarsCaiWORD <marstsaiforjob@gmail.com>
2023-08-07 19:33:41 +0800

    Options Pattern

--- FBAngularTW/Program.cs
+++ FBAngularTW/Program.cs
@@ -1,42 +1,15 @@
+using FBAngularTW;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var shortUrlProvider = new ShortUrlProvider(builder.Configuration);
+
+
 app.Run(ctx =>
 {
-    switch (ctx.Request.Host.Host)
-    {
-        case "fb.angular.tw":
-            ctx.Response.Redirect("https://www.facebook.com/groups/augularjs.tw");
-            break;
-
-        case "yt.angular.tw":
-            ctx.Response.Redirect("https://www.youtube.com/c/AngularUserGroupTaiwan/videos");
-            break;
-            
-        case "ts.angular.tw":
-            ctx.Response.Redirect("https://willh.gitbook.io/typescript-tutorial");
-            break;
-
-        case "vscode.angular.tw":
-            ctx.Response.Redirect("https://marketplace.visualstudio.com/items?itemName=doggy8088.angular-extension-pack");
-            break;
-
-        case "cli.angular.tw":
-            ctx.Response.Redirect("https://youtu.be/v4_YsDZbs3g");
-            break;
-
-        case "rx6.angular.tw":
-            ctx.Response.Redirect("https://youtu.be/BA1vSZwzkK8");
-            break;
-
-        case "install.angular.tw":
-            ctx.Response.Redirect("https://gist.github.com/doggy8088/15e434b43992cf25a78700438743774a");
-            break;
-
-        default:
-            ctx.Response.Redirect("https://www.facebook.com/will.fans");
-            break;
-    }
+    var shortUrl = shortUrlProvider.GetShortUrl(ctx.Request.Host.Host);
+    ctx.Response.Redirect(shortUrl);
     return Task.CompletedTask;
 });
 
