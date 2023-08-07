7da2c46eb6b6dcc46ccdfa44eaa831235439ecb2
Mowei <tso88206@gmail.com>
2023-08-07 15:06:26 +0800

    調整為設定讀取appsettings.json

---
+++ FBAngularTW/MySitesOptions.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace FBAngularTW
+{
+    public class MySitesOptions
+    {
+
+        public const string Position = "MySites";
+
+        public string Default { get; set; } = String.Empty;
+
+        public Dictionary<string, string> Site { get; set; } = new Dictionary<string, string>();
+    }
+}
