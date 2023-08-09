a861629f4d82911e3715b56c4bf78b467c88260e
王彥驊 <terry.wang@iisigroup.com>
2023-08-09 11:17:45 +0800

    移除不必要的註解程式碼

--- FBAngularTW/DomainUrlEntry.cs
+++ FBAngularTW/DomainUrlEntry.cs
@@ -1,19 +1,7 @@
 ﻿namespace FBAngularTW
 {
-    //public class DomainUrlEntry
-    //{
-    //    public string Domain { get; set; }
-
-    //    public string Url { get; set; }
-    //}
-
     public class DomainUrlConfig
     {
-        //public List<DomainUrlEntry> Entries
-        //{
-        //    get; set;
-        //}
-
         public Dictionary<string, string>? Mapping { get; set; }
     }
 
