1494864831ccdc9831e05dc4478fd3dc6c146fe9
michaelfangtw <yingchih.fang@gmail.com>
2023-08-12 21:34:13 +0800

    補上options pattern 和修改RedirectUrl格式

--- FBAngularTW/Program.cs
+++ FBAngularTW/Program.cs
@@ -1,13 +1,21 @@
+using FBAngularTW;
+using Microsoft.Extensions.Configuration;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(ctx =>
 {
     var host = ctx.Request.Host.Host;
-    var redirectUrl = builder.Configuration.GetValue<String>($"RedirectUrls:{host}");
-    var defaultUrl = builder.Configuration.GetValue<String>($"RedirectUrls:default");
-    redirectUrl= (redirectUrl != null)? redirectUrl:string.Format($"{defaultUrl}");    
-    ctx.Response.Redirect(redirectUrl);
+    host = "angular.tw";
+    var redirectUrlOptions = new List<RedirectUrlOption>();
+    builder.Configuration.GetSection(RedirectUrlOption.RedirectUrl).Bind(redirectUrlOptions);
+    var option = redirectUrlOptions.FirstOrDefault(x => x.host == host);            
+    if (option is not null)
+    {
+        ctx.Response.Redirect(option.url);
+    }
     return Task.CompletedTask;
 });
 
---
+++ FBAngularTW/RedirectUrlOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace FBAngularTW
+{
+    public class RedirectUrlOption
+    {
+        public const string RedirectUrl = "RedirectUrl";        
+        public string host { get; set; } = String.Empty;
+        public string url { get; set; } = String.Empty;
+    }
+
+
+}
