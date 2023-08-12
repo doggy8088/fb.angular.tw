0afcb339c6649c313d94053aa38d2f88c6a313e0
michaelfangtw <yingchih.fang@gmail.com>
2023-08-12 21:45:41 +0800

    移除多餘{}程式碼

--- FBAngularTW/Program.cs
+++ FBAngularTW/Program.cs
@@ -11,13 +11,8 @@ app.Run(ctx =>
     var redirectUrlOptions = new List<RedirectUrlOption>();
     builder.Configuration.GetSection(RedirectUrlOption.RedirectUrls).Bind(redirectUrlOptions);
     var option = redirectUrlOptions.FirstOrDefault(x => x.host == host);
-    if (option is null) {
-        option = redirectUrlOptions.FirstOrDefault(x => x.host == RedirectUrlOption.DefaultHost);
-    }
-    if (option is not null)
-    {
-        ctx.Response.Redirect(option.url);
-    }
+    if (option is null) option = redirectUrlOptions.FirstOrDefault(x => x.host == RedirectUrlOption.DefaultHost);    
+    if (option is not null) ctx.Response.Redirect(option.url);
     return Task.CompletedTask;
 });
 
